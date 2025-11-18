6434b529f559c327fbfa286e7763f2c5f8c99d14
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2025-11-18 23:49:02 +0530

    added utils

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -1,3 +1,16 @@
+# apps/utils/models.py
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    """
+    Ek abstract base class model jo har model mein 
+    'created_at' aur 'updated_at' fields jodta hai.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Abstract = True se Django is model ke liye koi table nahi banayega.
+        # Yeh sirf doosre models ko fields dene ke liye hai.
+        abstract = True
+        ordering = ['-created_at']
