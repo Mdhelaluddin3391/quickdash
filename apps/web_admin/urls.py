1669e9a306bfdb6b428914ce805745fabca4d3ff
Mdhelaluddin3391 <mdhelaluddin3391@gmail.com>
2025-11-28 01:09:48 +0530

    some update

--- apps/web_admin/urls.py
+++ apps/web_admin/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import AdminLoginView, AdminDashboardView
 
 urlpatterns = [
-    path('login/', AdminLoginView.as_view(), name='admin-login'),
-    path('', AdminDashboardView.as_view(), name='admin-panel'),
+    # Changed name='admin-login' to name='staff-login' to match the template
+    path('login/', AdminLoginView.as_view(), name='staff-login'),
+    path('', AdminDashboardView.as_view(), name='admin-dashboard'),
 ]
